fa56f7e8c40cc0ba4997f5b74d634d6c621817f3
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-02-01 17:13:15 +0000

    Finished Project

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'nblbeEIcn18L4VqN34Dgu5_pw-EIgJ_8oTJBT_neyIIvJGhemFdoN1RY42tA86r785YtQCSLeLIdZQIiHu3bu6gjGdYqN4lPE-l_EaOWXARVbrBZrB28qqAwjTedZXYx'; // Insert API key here.
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
